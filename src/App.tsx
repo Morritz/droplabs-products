d28b22863ad718ed4cc9290ad2435b46d23b6d68
Morritz <12800230+Morritz@users.noreply.github.com>
2025-12-05 20:21:36 +0100

    add routing with HomePage and ProductsPage components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import { BrowserRouter } from "react-router"
+import { BrowserRouter, Route, Routes } from "react-router"
+import HomePage from "./pages/HomePage"
+import ProductsPage from "./pages/ProductsPage"
 
 function App() {
   return (
     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductsPage />} />
+      </Routes>
     </BrowserRouter>
   )
 }
