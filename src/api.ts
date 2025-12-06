0a556cad076fa5a0391c625867ed675e3583d395
Morritz <12800230+Morritz@users.noreply.github.com>
2025-12-06 15:51:50 +0100

    add api module for fetching products with validation schema

---
+++ src/api.ts
@@ -0,0 +1,31 @@
+import ky from 'ky';
+import * as v from 'valibot';
+
+const kyInstance = ky.create({
+    prefixUrl: 'https://fakestoreapi.com',
+});
+
+const ProductSchema = v.object({
+    id: v.number(),
+    title: v.string(),
+    price: v.number(),
+    description: v.string(),
+    category: v.string(),
+    image: v.pipe(
+        v.string(),
+        v.nonEmpty(),
+        v.url(),
+    ),
+    rating: v.object({
+        rate: v.number(),
+        count: v.number(),
+    }),
+});
+
+export type Product = v.InferOutput<typeof ProductSchema>;
+
+export const getAllProducts = async (): Promise<Product[]> => {
+    const data = await kyInstance.get('products').json();
+    const products = v.parse(v.array(ProductSchema), data);
+    return products;
+};
