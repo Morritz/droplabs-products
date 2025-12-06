cc8183bd05348c95a9a39e04e2ff630dfecd9910
Morritz <12800230+Morritz@users.noreply.github.com>
2025-12-06 19:34:23 +0100

    place CartButton in AppBar

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,13 +1,19 @@
 import { AppBar as AppBarMUI, Toolbar, Box } from '@mui/material';
 import { AppBarButton } from './AppBarButton';
+import { CartButton } from './CartButton';
 
 export function AppBar() {
     return (
         <AppBarMUI position="sticky">
             <Toolbar>
-                <Box>
-                    <AppBarButton title="Home" path="/" />
-                    <AppBarButton title="Products" path="/products" />
+                <Box display={"flex"} justifyContent={"space-between"} width={"100%"} alignItems={"center"}>
+                    <Box>
+                        <AppBarButton title="Home" path="/" />
+                        <AppBarButton title="Products" path="/products" />
+                    </Box>
+                    <Box>
+                        <CartButton />
+                    </Box>
                 </Box>
             </Toolbar>
         </AppBarMUI>
