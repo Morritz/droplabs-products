64d7f4128fa29cd078f08b3cad890a4b7e962bfd
Morritz <12800230+Morritz@users.noreply.github.com>
2025-12-07 09:49:08 +0100

    add empty cart message

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,16 +1,17 @@
-import { Box, Container } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import { useCart } from "../store";
 import { ProductCardCart } from "../components/ProductCardCart";
 
 export function CartPage() {
     const cart = useCart((state) => state.cart);
     const data = Object.values(cart);
+    const cartIsNotEmpty = data.length > 0;
     return (
         <Container>
             <Box padding={4} gap={4} display={"flex"} flexDirection={"column"}>
-                {data.map((cartItem) => (
+                {cartIsNotEmpty ? data.map((cartItem) => (
                     <ProductCardCart key={cartItem.id} cartItem={cartItem} />
-                ))}
+                )) : <Typography variant="h4">Koszyk jest pusty</Typography>}
             </Box>
         </Container>
     );
