8db7db1344e910938e5097c5d2adcf0b80e19ca0
Morritz <12800230+Morritz@users.noreply.github.com>
2025-12-07 10:18:50 +0100

    use sorting in ProductsPage

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,14 +1,21 @@
 import {
     Container,
     Box,
+    ButtonGroup,
+    Button,
 } from '@mui/material';
 import { ProductCard } from '../components/ProductCard';
 import { useGetAllProductsQuery } from '../hooks/useGetAllProductsQuery';
 import { ProductsLoadingError } from '../components/ProductsLoadingError';
 import { CircularLoadingProgress } from '../components/CircularLoadingProgress';
+import { useSortProducts } from '../hooks/useSortProducts';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 
 export function ProductsPage() {
     const { data, isLoading, error } = useGetAllProductsQuery();
+
+    const { sortedProducts, sortField, sortOrder, sortBy } = useSortProducts(data);
 
     if (isLoading)
         return <CircularLoadingProgress />
@@ -19,8 +26,32 @@ export function ProductsPage() {
 
     return (
         <Container>
+            <Box p={4}>
+                <ButtonGroup variant="contained" sx={{ mb: 3 }}>
+                    <Button
+                        onClick={() => sortBy('default')}
+                        color={sortField === 'default' ? 'primary' : 'inherit'}
+                    >
+                        Default
+                    </Button>
+                    <Button
+                        onClick={() => sortBy('title')}
+                        color={sortField === 'title' ? 'primary' : 'inherit'}
+                        endIcon={sortField === 'title' && (sortOrder === 'asc' ? <ArrowUpwardIcon /> : <ArrowDownwardIcon />)}
+                    >
+                        Title
+                    </Button>
+                    <Button
+                        onClick={() => sortBy('price')}
+                        color={sortField === 'price' ? 'primary' : 'inherit'}
+                        endIcon={sortField === 'price' && (sortOrder === 'asc' ? <ArrowUpwardIcon /> : <ArrowDownwardIcon />)}
+                    >
+                        Price
+                    </Button>
+                </ButtonGroup>
+            </Box>
             <Box padding={4} gap={4} display={"flex"} flexDirection={"column"}>
-                {data.map((product) => (
+                {sortedProducts.map((product) => (
                     <ProductCard key={product.id} product={product} />
                 ))}
             </Box>
